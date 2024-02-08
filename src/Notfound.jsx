fd2a69a46d2160b596df0692f06f31d181ca35bf
go <goito.722@icloud.com>
2024-02-08 23:35:31 +0900

    fix notfound

---
+++ src/Notfound.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom"
+export const NotFound = () => {
+    return(
+        <>
+        <h1>Page404</h1>
+        <Link to="/">トップページに戻る</Link>
+        </>
+    )
+}
--- src/page404.jsx
+++
@@ -1,7 +0,0 @@
-export const Page404 = () => {
-    return(
-        <>
-        <h1>Page404</h1>
-        </>
-    )
-}
